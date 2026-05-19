4c5529a5ac18db219af4e6e1a5a8f0ad26c55009
Kristin <kutsiepudy3841@gmail.com>
2026-05-19 11:39:09 -0700

    Add game state management functions and object

--- state.js
+++ state.js
@@ -1,1 +1,17 @@
+export const gameState = {
+  mode: "exploring",
+  battlePhase: null,
+  dialogueActive: false
+};
 
+export function setMode(newMode) {
+  gameState.mode = newMode;
+};
+
+export function setPhase(phase) {
+  gameState.battlePhase = phase;
+};
+
+export function setDialogueActive(active) {
+  gameState.dialogueActive = active
+}'
