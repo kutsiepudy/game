6ba432be25946dc3f3d09b62029644d3d55a030a
Kristin <kutsiepudy3841@gmail.com>
2026-08-12 21:31:17 -0700

    Refactor enemy stats into enemyTypes function
    Refactor enemy stats and types into a single function that creates enemy objects based on type.

--- enemy.js
+++ enemy.js
@@ -1,8 +1,27 @@
-export const enemyStats = {
-  health: 19,
+export function enemyTypes(type = "basic") {
+  const enemyClasses = {
+    basic: { health: 20, damage: 2, name: "small" },
+    strong: { health: 30, damage: 4, name: "big" },
+    boss: { health: 60, damage: 10, name: "huge"}
+  };
 
-  takeDamage(amount) {
-    this.health -= amount;
-    if (this.health < 0) this.health = 0;
-  }
-};
+ const config = enemyTypes[types] || enemyClasse.basic;
+
+ return {
+   name: config.name,
+   health: config.health,
+   maxHealth: config.health,
+   damage: config.damage,
+
+   takeDamage(amount) {
+     this.health -= amount;
+     if (this.health <= 0) this.health = 0;
+   },
+
+   reset() {
+     this.health = this.maxHealth;
+   }
+ };
+}
+
+export let enemyStats = creatEnemy("basic")
